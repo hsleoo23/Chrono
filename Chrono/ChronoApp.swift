d08db22c7b71bf365ba835e2d7189e1a30dc9af4
hsLeooo <1726778203@qq.com>
2025-06-11 15:08:09 +0800

    Initial Commit

---
+++ Chrono/ChronoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoApp.swift
+//  Chrono
+//
+//  Created by 韩澍 on 2025/6/11.
+//
+
+import SwiftUI
+
+@main
+struct ChronoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronoTests/ChronoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronoTests.swift
+//  ChronoTests
+//
+//  Created by 韩澍 on 2025/6/11.
+//
+
+import Testing
+@testable import Chrono
+
+struct ChronoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
